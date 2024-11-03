66bccf73c524949a8cdbef8357064f58137fd6f9
felipesuarezB <felipesuarezbe@hotmail.com>
2024-11-02 19:58:44 -0500

    Creacion de unit test

--- examen-semana4/src/app/vehiculo/vehiculo-listar/vehiculo-listar.component.spec.ts
+++ examen-semana4/src/app/vehiculo/vehiculo-listar/vehiculo-listar.component.spec.ts
@@ -1,17 +1,23 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { VehiculoListarComponent } from './vehiculo-listar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehiculoService } from '../vehiculo.service';
+import { Vehiculo } from '../vehiculo';
+import { faker } from '@faker-js/faker/.';
 
 describe('VehiculoListarComponent', () => {
   let component: VehiculoListarComponent;
   let fixture: ComponentFixture<VehiculoListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListarComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehiculoListarComponent ],
+      providers: [ VehiculoService ],
     })
     .compileComponents();
   }));
@@ -19,10 +25,31 @@ describe('VehiculoListarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListarComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const vehiculo = new Vehiculo(
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.number.int(),
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+      component.vehiculos.push(vehiculo);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a table with three rows plus the header', () => {
+    const tableRows = debug.queryAll(By.css('table tbody tr'));
+    expect(tableRows.length).toBe(3);
   });
 });
